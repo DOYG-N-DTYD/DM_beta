d9c25319fdd1cc8fbacc68d15b2d75d2637fad4d
DOYG-N-DTYD <112571712+DOYG-N-DTYD@users.noreply.github.com>
2023-09-07 10:04:40 +0000

    testing frontend for chhat

--- src/app/test-page/test-page.component.ts
+++ src/app/test-page/test-page.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+export class Phone{
+  constructor(public title: string, 
+              public price: number, 
+              public company: string)
+  { }
+}
+
 @Component({
   selector: 'app-test-page',
   templateUrl: './test-page.component.html',
   styleUrls: ['./test-page.component.sass']
 })
 export class TestPageComponent {
-
+  title: string = "";
+  price: number = 0;
+  company: string = "";
+   
+  phones: Phone[] = [];
+  companies: string[] = ["Apple", "Huawei", "Xiaomi", "Samsung", "LG", "Motorola", "Alcatel"];
+   
+  addPhone(){
+      this.phones.push(new Phone(this.title, this.price, this.company));
+  }
 }
