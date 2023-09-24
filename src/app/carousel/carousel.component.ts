3f49205bce726b086425d363c587b9ecfe405dcf
DOYG-N-DTYD <miroslawzborowski1105@gmail.com>
2023-09-24 18:20:05 +0200

    ok, but small issues ith windows width

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { elementAt } from 'rxjs';
 
 @Component({
   selector: 'app-carousel',
@@ -24,65 +25,94 @@ export class CarouselComponent {
 
   constructor() { }
 
-  leftArrow() {return document.getElementById('carouselLeft') as HTMLElement;}
-  rightArrow() {return document.getElementById('carouselRight') as HTMLElement;}
-  container() {return document.getElementsByClassName('container');}
+  leftArrow() { return document.getElementById('carouselLeft') as HTMLElement; }
+  rightArrow() { return document.getElementById('carouselRight') as HTMLElement; }
+  container() { return document.getElementsByClassName('container'); }
   imageLeftClick() {
     console.log("Left click");
     console.log(this.leftArrow());
   }
 
-  ngOnInit(){
-    let imageWidth: number = 300;
-
+  ngOnInit() {
     this.containerWithImages = document.getElementById('container') as HTMLDivElement;
     this.carousel = document.getElementById('carousel') as HTMLDivElement;
+    this.containerWithImages.setAttribute("style", "width:" + (this.containerWithImages.offsetWidth - 300) + "px"); //300
 
     this.imagesInCourusel = this.initFIFOforImages();
-    
-    var intervalID = setInterval(() => {
-      let tempImageObg = this.imagesInCourusel.shift(); //this.imageInCourusel
-      this.containerWithImages.appendChild(tempImageObg);//this.imageInCourusel);
-      this.containerWithImages.setAttribute("style", "width:" + (this.containerWithImages.offsetWidth - imageWidth) + "px"); //containerWithImagesWidth - imageWidth first time
-      this.moveImage(tempImageObg);
-    },3500)
+    this.appentChildsToContainerWithImages();
+    this.startCarousel();
+  }
+ 
+  appentChildsToContainerWithImages(){
+    for(let child of this.imagesInCourusel){
+      this.containerWithImages.appendChild(child);
+    }
+  }
+  clearContainerOfNodes(){
+    for(let child of this.imagesInCourusel){
+      //this.containerWithImages.appendChild(child);
+      this.containerWithImages.removeChild(child); // Чистить ноды нужно когда ширина экрана меняется
+    }
   }
 
-  
   initFIFOforImages() {
     var arrayOfDivsWithImages: HTMLElement[] = [];
     this.pathsToImageInCourusel.forEach(imgPath => {
       let arrayOfStrings: string[] = imgPath.split("/");  // /.../.../.../
       let imgName = arrayOfStrings[arrayOfStrings.length - 1].split(".")[0]; // balka.jpg')".split(".")[0]
       arrayOfDivsWithImages.push(this.setImageProperties(document.createElement(imgName) as HTMLElement, imgPath));
-      console.log(imgPath);
     });
     return arrayOfDivsWithImages;
   }
-  setImageProperties(divWithImage: HTMLElement, imgPath:string) {
+  setImageProperties(divWithImage: HTMLElement, imgPath: string) {
     divWithImage.style.backgroundImage = "url('" + imgPath + "')";
     //"url('/assets/images/Products/balka.jpg')";
     divWithImage.style.width = 300 + 'px';
     divWithImage.style.height = 300 + 'px';
     divWithImage.style.position = 'absolute';
-    divWithImage.style.left = '-300px';  // change to dynamic width of image
-    //divWithImage.style.transitionTimingFunction = 'linear';
-    //divWithImage.style.transitionDuration = '0.5s';//'5s';
-    //divWithImage.style.transition = 'all 0.5s';
+    divWithImage.style.left = '-300px';
     return divWithImage;
   }
+  startCarousel() {
+    var tempImageObg;
+    var intervalForChild = setInterval(() => {
+      tempImageObg = this.imagesInCourusel.shift();
+      //this.containerWithImages.appendChild(tempImageObg);
+      this.moveImage(tempImageObg);
+      if (tempImageObg.localName == "rdoska") {
+        
+        clearInterval(intervalForChild);
+        this.clearContainerOfNodes();
+        this.imagesInCourusel = this.initFIFOforImages();
+        this.appentChildsToContainerWithImages();
+        this.startCarousel();
+      }
+      console.log("INTERVAL START CAROUSEL");
+      // if (this.containerWithImages.childNodes.length == 8) {
+      //   // for (var child in this.containerWithImages.childNodes) {
+      //   //   console.log(1111, this.containerWithImages.childNodes[child]);
+      //   //   this.containerWithImages.removeChild(this.containerWithImages.childNodes[child]);
+      //   // }
+      //   //rdoska
+      //   clearInterval(intervalForChild);
+      // }
+    }, 3500)
+
+    //this.startCarousel();
+  }
   moveImage(imageInCourusel: any) {
-    var intervalID = setInterval(() => {
+    var intervalImageMove = setInterval(() => {
       var speed = 1;
       var offsetleft = parseInt(imageInCourusel.offsetLeft);//||0;
       var offsetright = window.innerWidth - imageInCourusel.offsetLeft - imageInCourusel.offsetWidth
       imageInCourusel.style.left = (offsetleft + speed) + 'px';
+      console.log("INTERVAL IMAGE MOVE");
       if (offsetright <= (-1 * imageInCourusel.offsetWidth)) {
-        clearInterval(intervalID);
-        this.containerWithImages.removeChild(this.containerWithImages.childNodes[0]);//this.imageInCourusel); // ??
+        clearInterval(intervalImageMove);
+        //this.containerWithImages.removeChild(this.containerWithImages.childNodes[0]);//this.imageInCourusel); // ??
+        this.imagesInCourusel.push(imageInCourusel);
       }
-    },10)
-
+    }, 10)
   }
   // moveImage() {
   //   var intervalID = setInterval(() => {
